a31b1ad4d4105c970d7fb7f51d18362eb841233b
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-08-16 23:43:48 -0300

    feat: added more scanry test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -10,3 +10,32 @@ describe('template spec', () => {
     
   })
 })
+
+describe('should testing all routes on adopet website', () => {
+   it('should visit home page and click on the ver pets button', () => { 
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.contains('a', 'Ver pets disponíveis para adoção').click()
+
+   })
+
+   it('should test all the headers buttons', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+    cy.get('a[aria-label="Tela inicial"]').click();
+    cy.get('a[aria-label="Ir para Mensagens"]').click();
+   })
+
+   it('should visit the login page', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/login')
+   })
+
+   it('should visit the home page', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home')
+   })
+   
+
+   it('should visit the home page and click on the Falar com responsável button', () => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/home')
+    cy.get('a[aria-label="Falar com responsável"]').first().click();
+   })
+
+})
