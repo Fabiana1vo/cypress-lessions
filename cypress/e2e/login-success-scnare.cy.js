21ca7129abb9a6f43cd55317bb3b38fbf566653a
Fabiana1vo <fabianasouzaivo@icloud.com>
2025-08-17 18:28:52 -0300

    feat: create suit tests to login page

---
+++ cypress/e2e/login-success-scnare.cy.js
@@ -0,0 +1,17 @@
+describe('login page suit tests', () => {
+ beforeEach(() => {
+    cy.visit('https://adopet-frontend-cypress.vercel.app/')
+  })
+
+  it('should access login page', () => { 
+    cy.get('[data-test="login-button"]').click()
+  })
+
+  it('should add login information', () => {
+    cy.get('[data-test="login-button"]').click()
+    cy.get('[data-test="input-loginEmail"]').type('ana@email.com')
+    cy.get('[data-test="input-loginPassword"]').type('Senha123')
+    cy.get('[data-test="submit-button"]').click()
+  })
+
+})
